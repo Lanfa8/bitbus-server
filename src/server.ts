dac8c7233202a686b1943ee6928aa9b0b2510b2d
Lanfa8 <alanfan139@gmail.com>
2024-05-09 21:45:45 -0300

    feat: cadastro básico de artefatos

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,39 @@
 import express from 'express';
 import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client'
+import { create } from 'domain';
 
 const app = express();
+app.use(express.json());
+const prisma = new PrismaClient()
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Show de bola Márcio!');
+});
+
+app.post("/artefato", async (req: Request, res: Response) => {
+  // Código para criar um artefato
+  await prisma.artefato.create({
+    data: {
+      nome: req.body.nome,
+      descricao: req.body.descricao,
+      origem: req.body.origem,
+      dataInclusao: new Date(),
+      categoria: {
+        create: {
+          descricao: req.body.categoria
+        }
+      }
+    }
+  });
+
+  const allArtefatos = await prisma.artefato.findMany({
+    include: {
+      categoria: true,
+    },
+  })
+  res.send(allArtefatos);
+  await prisma.$disconnect()
 });
 
 app.listen(3000, () => {
